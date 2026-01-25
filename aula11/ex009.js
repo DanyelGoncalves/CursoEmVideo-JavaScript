70ae9dc3cdcf38cb72f8c6122a69dcb7076d3e1b
Danyel Gonçalves <danyel.martins@hotmail.com>
2026-01-25 17:29:17 -0300

    Aula 011 Funções Parte 1

---
+++ aula11/ex009.js
@@ -0,0 +1,20 @@
+console.log(`O console funcionou`)
+
+var vel = 61.5
+
+if (vel > 60) { 
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+
+} else { 
+
+console.log(`Dentro da Velocidade permitida A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Dirija sempre usando cinto de segurança!`) }
+
+
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil') { 
+    console.log(`Você é Gringo!`)
+} else {
+    console.log(`Você é Brasileiro!`)
+}
