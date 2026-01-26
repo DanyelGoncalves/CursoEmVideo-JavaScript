d9af8fb4f2937b814d07cea1b971b7ab43512629
Danyel Gonçalves <danyel.martins@hotmail.com>
2026-01-25 21:47:53 -0300

    modulo D
    aula 11, 12, 13 e 14 (exercicios)
    necessidade de adequacao das imgs com editor de img e redimensionamento 250x

---
+++ aula012/ex011.js
@@ -0,0 +1,8 @@
+var idade = 18
+if (idade < 16){
+    console.log('Menos de 16 - Não vota')
+    } else if (idade < 18) { 
+        console.log('Maior de 16 - voto opcional')
+    }  else { 
+        console.log('Idade igual ou acima de 18 - Voto Obrigatório!') 
+    }
