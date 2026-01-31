74d74df2b89477cee5b1d531d1ea408fa5245d57
Danyel Gonçalves <danyel.martins@hotmail.com>
2026-01-30 23:48:14 -0300

    aulas modulo E

---
+++ aula13-laços/ambiente.js
@@ -0,0 +1,7 @@
+var c = 1
+do {
+    console.log(`Naila minha neném essa é a ${c}º vez que digo que te amo`)
+    c++
+} while (c <= 100)
+
+    // repeticao usando DO e WHILE com condição através de contador.
---
+++ aula14lacos-for/ex015lacosFOR.js
@@ -0,0 +1,12 @@
+var c= 1
+while (c<=10) {
+   
+    console.log(`No While valor de C é ${c}`)
+    c++
+}
+
+// mesmo em for:
+
+for (c=1;c <=5;c++) {
+    console.log(`No laço FOR o valor de C é ${c}`)
+}
